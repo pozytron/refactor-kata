204cbc3659330b7f268bb3a865d9f06bc4bfbf9f
pozytron <michal.tymula@coderslab.pl>
2021-07-05 21:36:10 +0200

    catalogs refactor

---
+++ src/components/Statement/index.js
@@ -0,0 +1,26 @@
+import {statement} from "../../utils/statement";
+import {customer} from "../../data/customer";
+import {movies} from "../../data/movies";
+
+
+const Statement = () => {
+    statement(customer,movies)
+    return (
+        <>
+            <h1>Rental Record for <em>martin</em></h1>
+            <table>
+                <tr>
+                    <td>Ran</td>
+                    <td>3.5</td>
+                </tr>
+                <tr>
+                    <td>Trois Couleurs: Bleu</td>
+                    <td>2</td>
+                </tr>
+            </table>
+            <p>Amount owed is <em>5.5</em></p>
+            <p>You earned <em>2</em> frequent renter points</p>
+        </>
+    )
+}
+export default Statement;
